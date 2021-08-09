9c3ce95d0f46647c595a5fdb70ceb8d09fad927f
okulist100 <adikts@mail.ru>
2021-08-09 18:04:36 +0600

    fixed error after email confirmation!!

--- src/main/java/com/shtraf/bir/controllers/RegistrationController.java
+++ src/main/java/com/shtraf/bir/controllers/RegistrationController.java
@@ -5,10 +5,12 @@ import com.shtraf.bir.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -25,12 +27,21 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model){
-        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())){
+    public String addUser(
+            @RequestParam("password2") String passwordConfirm,
+            @Valid User user,
+            BindingResult bindingResult,
+            Model model
+    ){
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+        if (isConfirmEmpty){
+            model.addAttribute("password2Error" , "Password confirmation cannot be empty");
+        }
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirm)){
             model.addAttribute("passwordError","Passwords are different");
         }
 
-        if (bindingResult.hasErrors()){
+        if (isConfirmEmpty || bindingResult.hasErrors()){
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
 
             model.mergeAttributes(errors);
